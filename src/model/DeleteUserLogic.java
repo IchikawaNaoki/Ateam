b76db3d42e27604ad8c51e742b42ffc50012b980
KeiShionuma <shionuma-kei@mgt-net.com>
2019-06-19 11:18:34 +0900

    Delete系とRegister系を変更しました。

--- src/model/DeleteUserLogic.java
+++ src/model/DeleteUserLogic.java
@@ -11,17 +11,28 @@ import dao.ConnDbDao;
 //-------------------------------------------------------------------------------------------------------------
 public class DeleteUserLogic
 {
-	public boolean execute(User user)
+	public boolean deleteExecute(User user)
 	{
-			//DB接続し、情報合致しているかの判定を行い結果を返す
-			return new ConnDbDao().ConnDbCollation(user);
+		//入力判定
+		return (deleteCheck(user));
 	}
-	
+
 	public void deleteDone(User user)
 	{
 		//DBコネクトインスタンス作成
 		ConnDbDao conn = new ConnDbDao();
 		//削除メソッド呼び出し
 		conn.ConnDbDelete(user);
+	}
+	//IDとPASS照合
+	public boolean deleteCheck(User user)
+	{//IDとPASSが入力されているかを判定
+		System.out.println("削除情報の照合を行います");
+		if((user.getId() != 0) && (!user.getPass().equals("")))
+		{
+			//DB接続し、情報合致しているかの判定を行い結果を返す
+			return new ConnDbDao().ConnDbCollation(user);
+		}
+		return false;
 	}
 }
